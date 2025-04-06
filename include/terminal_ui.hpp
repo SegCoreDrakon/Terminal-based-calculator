96ccb3dd9ac3db03fc5629b1134a7e2debbc7608
SegCoreDrakon <segcoredrakon@gmail.com>
2025-04-06 10:35:47 +0200

    add lost include
    re-add calculator.hpp and terminal_ui.hpp lost after a merge conflict
    resolv

---
+++ include/terminal_ui.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file terminal_ui.hpp
+ * @brief the header of the UI system
+ * @author SegCoreDrakon
+ * @date 2025-03-31
+ * @version 0.0.14
+ */
+
+#pragma once
+#ifndef TERMINAL_UI_HPP
+#define TERMINAL_UI_HPP
+
+class UI {
+public:
+    UI();
+    ~UI();
+
+    void menu_display() const;
+    void logs_display() const;
+    void number_display() const;
+    void result_display() const;
+    void clear_screen() const;
+
+    void move(int A, int G, bool B);
+};
+
+#endif // TERMINAL_UI_HPP
