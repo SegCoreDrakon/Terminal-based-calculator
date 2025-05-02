3f13da8007edcb079d481cb19d5563277d7eb8cf
SegCoreDrakon <segcoredrakon@gmail.com>
2025-05-02 23:38:18 +0200

    Delete doc directory

--- doc/html/search/files_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['terminal_5fui_2ecpp_0',['terminal_ui.cpp',['../terminal__ui_8cpp.html',1,'']]],
-  ['terminal_5fui_2ehpp_1',['terminal_ui.hpp',['../terminal__ui_8hpp.html',1,'']]]
-];
--- doc/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['calc_0',['calc',['../save__system_8cpp.html#a5140dee23d40cbc8672bb7c9adcb52be',1,'save_system.cpp']]]
-];
